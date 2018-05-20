3f1398c922e5d360dd9a417a2e9e607f3c030107
CosmicDan <dconnolly.au@gmail.com>
2018-05-21 06:12:12 +1000

    init script for symlinking camera libs

---
+++ rootdir/init.treblecompat.sh
@@ -0,0 +1,27 @@
+#! /vendor/bin/sh
+
+# common functions
+symlink() {
+	ln -sn $1 $2;
+}
+
+# mount system (root for our device) and vendor as rw
+# NOTE - Requires dm_verity disabled in kernel
+mount -o remount,rw /
+mount -o remount,rw /vendor
+
+
+# symlink camera stuff
+symlink /vendor/etc/morpho_lowlight4.0.xml /system/etc/morpho_lowlight4.0.xml
+symlink /vendor/lib/libMiCameraHal.so /system/lib/libMiCameraHal.so
+symlink /vendor/lib/libdualcameraddm.so /system/lib/libdualcameraddm.so
+symlink /vendor/lib/libjni_dualcamera.so /system/lib/libjni_dualcamera.so
+symlink /vendor/lib/libmorpho_groupshot.so /system/lib/libmorpho_groupshot.so
+symlink /vendor/lib/libmorpho_memory_allocator.so /system/lib/libmorpho_memory_allocator.so
+symlink /vendor/lib/libmorpho_panorama.so /system/lib/libmorpho_panorama.so
+symlink /vendor/lib/libmorphohht4.0.so /system/lib/libmorphohht4.0.so
+symlink /vendor/lib/libtrueportrait.so /system/lib/libtrueportrait.so
+
+# remount system and vendor as ro
+mount -o remount,ro /
+mount -o remount,ro /vendor
