34d391baeab54f1dfa4183cd2566a483b4139c48
CosmicDan <dconnolly.au@gmail.com>
2018-06-03 04:11:25 +1000

    Convert flash_image to ZIP proxy and use proper sparse image detection
    Change-Id: I1ceb84aac955ebb788157eac1dfdc700315d5ea0

--- recovery/root/tissot_manager/flash_image.sh
+++ recovery/root/tissot_manager/flash_image.sh
@@ -1,21 +1,34 @@
 #!/sbin/sh
+### flash_image script
 
-### simg2img bootstrap
-
+# only source it, we set our own OUT_FD
 source /tissot_manager/tools.sh
-# note - calling this script from cmd doesn't support ui_print for some reason
 
-sourceFile="$1"
-targetMount="$2"
+OUT_FD=/proc/$$/fd/$2
 
+abort() {
+	ui_print
+	ui_print "[!] Error: $1"
+	ui_print
+	exit 1
+}
+
+
+sourceFile=`cat /tmp/flash_image_source`
+targetMount=`cat /tmp/flash_image_target | sed 's|;||'`
+
+# verify source
 if [ ! -f "$sourceFile" ]; then
-	echo "[!] Source file not found!"
-	return;
+	abort "Source file not found"
 fi
+
+ui_print
+ui_print "[#] Starting flash from $sourceFile"
 
 targetBlock=
 targetType=
 isFilesystem=false
+
 
 # look for mount point and type from recovery.fstab
 while read LINE; do
@@ -28,13 +41,13 @@ while read LINE; do
 	if [ "$entryMountPoint" = "$targetMount" ]; then
 		targetBlock=$(echo "$LINE" | awk '{ print $1 }')
 		targetType=$(echo "$LINE" | awk '{ print $3 }')
-		echo "[i] Target block is $targetBlock of type $targetType"
+		ui_print "    [i] Target = $targetBlock [$targetType]"
 		break;
 	fi
 done < /etc/recovery.fstab
 
 if [ "$targetBlock" = "" -o "$targetType" = "" ]; then
-	# look for mount point and type from twrp.flags
+	# wasn't found in fstab - look for mount point and type from twrp.flags
 	while read LINE; do
 		firstChar=`echo "$LINE" | head -c 1`
 		if [ "$firstChar" = "#" -o "$firstChar" = "" ]; then 
@@ -45,37 +58,33 @@ if [ "$targetBlock" = "" -o "$targetType" = "" ]; then
 		if [ "$entryMountPoint" = "$targetMount" ]; then
 			targetBlock=$(echo "$LINE" | awk '{ print $3 }')
 			targetType=$(echo "$LINE" | awk '{ print $2 }')
-			echo "[i] Target block is $targetBlock of type $targetType"
+			ui_print "    [i] Target = $targetBlock [$targetType]"
 			break;
 		fi
 	done < /etc/twrp.flags
 fi
 
-# now check if this block device is actually a filesystem
-while read LINE; do
-	firstChar=`echo "$LINE" | head -c 1`
-	if [ "$firstChar" = "#" -o "$firstChar" = "" ]; then 
-		# skip comments and blank lines
-		continue
-	fi
-	entryBlock=$(echo "$LINE" | awk '{ print $3 }')
-	if [ "$entryBlock" = "$targetBlock" ]; then
-		entryType=$(echo "$LINE" | awk '{ print $2 }')
-		if [ "$entryType" = "ext4" -o "$entryType" = "f2fs" ]; then
-			isFilesystem=true
-		fi
-	fi
-done < /etc/twrp.flags
-
-if [ "$isFilesystem" = "true" ]; then
-	echo "[i] Flash mode is filesystem"
-	echo "[#] Running simg2img $sourceFile $targetBlock ..."
-	simg2img $sourceFile $targetBlock
-	exit $?
-else
-	echo "[i] Flash mode is raw"
-	echo "[#] Running dd if=$sourceFile of=$targetBlock ..."
-	dd if=$sourceFile of=$targetBlock
-	exit $?
+if [ "$targetBlock" = "" -o "$targetType" = "" ]; then
+	abort "Could not find flash target"
 fi
 
+# look for sparse image magic
+sparse_magic=`hexdump -e '"%02x"' -n 4 "$sourceFile"`
+
+if [ "$sparse_magic" = "ed26ff3a" ]; then
+	ui_print "    [i] Detected sparse image"
+	ui_print "    [#] Flashing via simg2img..."
+	simg2img $sourceFile $targetBlock
+	result=$?
+else
+	ui_print "    [i] Sparse magic not found, assuming raw image"
+	ui_print "    [#] Flashing via dd..."
+	dd if=$sourceFile of=$targetBlock
+	result=$?
+fi
+
+# cleanup
+rm /tmp/flash_image_source
+rm /tmp/flash_image_target
+
+exit $result
