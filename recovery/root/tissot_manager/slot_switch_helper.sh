5e08bc9916d5df19339a2341267a05542be2fb03
CosmicDan <dconnolly.au@gmail.com>
2018-06-03 14:25:20 +1000

    TWRP slot change fixes for dualboot and blank vendor mounts breaking boot HAL
    Change-Id: I082d6cf1e74aab35082294da326cc15a7c6d41d1

---
+++ recovery/root/tissot_manager/slot_switch_helper.sh
@@ -0,0 +1,27 @@
+#!/sbin/sh
+# 
+
+if [ "$2" != "real" ]; then
+	umount -f /system
+	umount -f /vendor
+	umount -f /sdcard
+	umount -f /data
+	$0 $1 "real" &
+	exit
+fi
+
+while true; do
+	userdata_link=`readlink /dev/block/bootdevice/by-name/userdata`
+	slot_suffix=`echo -n $userdata_link | tail -c 1`
+	if [ "$slot_suffix" = "$1" ]; then
+		break
+	fi
+	sleep 0.1
+done
+
+mount /dev/block/bootdevice/by-name/userdata_$1 /data
+if [ ! -d /data/media ]; then
+	mkdir /data/media
+	chmod 0770 /data/media
+fi
+mount -o bind /data/media /sdcard
